52672b593aa78d697c9bef9393711cc9ef539d02
root <root@ubuntu-s-4vcpu-16gb-amd-nyc1.tail6530ef.ts.net>
2026-06-16 02:16:43 +0000

    Fix admin/login 500: Add missing PageConfigProvider and 39 other @davincios/ui exports

--- scripts/create-ui-stub.js
+++ scripts/create-ui-stub.js
@@ -37,7 +37,32 @@ const comps = {
   Restore: R, RenderFieldsToDiff: R, SearchFilter: R, ListControls: R, ColumnSelector: R,
   DownloadAsCSV: R, WhereBuilder: R, RenderCustomComponents: R, UploadGallery: R, ReactSelect: R,
   FieldDescription: R, FormatDateTime: R, SortArrow: R, LinkToDoc: R, Banner: R,
-  FieldDiffContainer: R, FieldDiffLabel: R, FieldLabel: R
+  FieldDiffContainer: R, FieldDiffLabel: R, FieldLabel: R,
+  // --- Missing components identified by auditing all @davincios/ui imports across packages/next/dist ---
+  PageConfigProvider: C,       // CRITICAL: imported by views/Root/index.js (causes 500 on /admin/login)
+  MinimizeMaximizeIcon: R,     // imported by views/API/index.client.js
+  ViewDescription: R,          // imported by Document/renderDocumentSlots.js
+  LivePreviewProvider: C,      // imported by views/Document/index.js
+  LoadingOverlay: R,           // imported by views/Logout/LogoutClient.js, Version/SelectComparison/VersionDrawer/index.js
+  OperationProvider: C,        // imported by Dashboard/Default/ModularDashboard/WidgetConfigDrawer.js
+  ShimmerEffect: R,            // imported by Dashboard/Default/ModularDashboard/renderWidget/RenderWidget.js, WidgetConfigDrawer.js, Version/RenderFieldsToDiff/RenderVersionFieldsToDiff.js
+  Popup: R,                    // imported by Dashboard/Default/ModularDashboard/index.client.js, elements/Nav/SettingsMenuButton/index.js
+  PopupList: R,                // imported by Dashboard/Default/ModularDashboard/index.client.js, Version/Restore/index.js
+  XIcon: R,                    // imported by Dashboard/Default/ModularDashboard/index.client.js
+  RadioGroupField: R,          // imported by views/Account/ToggleTheme/index.js
+  PillSelector: R,             // imported by views/Version/SelectLocales/index.js
+  RenderTitle: R,              // imported by elements/DocumentHeader/index.js
+  NavGroup: R,                 // imported by elements/Nav/index.client.js
+  Logout: R,                   // imported by elements/Nav/index.js
+  FolderTypeField: R,          // exported from exports/client.js
+  QueryPresetsAccessCell: R,   // exported from exports/client.js
+  QueryPresetsColumnField: R,  // exported from exports/client.js
+  QueryPresetsColumnsCell: R,  // exported from exports/client.js
+  QueryPresetsGroupByCell: R,  // exported from exports/client.js
+  QueryPresetsGroupByField: R, // exported from exports/client.js
+  QueryPresetsWhereCell: R,    // exported from exports/client.js
+  QueryPresetsWhereField: R,   // exported from exports/client.js
+  SlugField: R                 // exported from exports/client.js
 };
 const hooks = {
   useNav: H, useTranslation: function () { return { t: function (s) { return s }, i18n: {} } },
@@ -47,7 +72,12 @@ const hooks = {
   useFormDependency: H, useTheme: H, usePreferences: H, useGlobals: H, useTitle: H,
   useAllConfig: H, useAllTheme: H, useAllAuth: H, useAllDocumentInfo: H, useAllStepNav: H,
   useAllPreferences: H, useAllTranslations: H, useAllLocales: H, useConfigByPath: H,
-  useAllFieldTypes: H, useFieldType: H, useGlobalsByPath: H
+  useAllFieldTypes: H, useFieldType: H, useGlobalsByPath: H,
+  // --- Missing hooks identified by auditing all @davincios/ui imports across packages/next/dist ---
+  useRouteTransition: H,       // imported by views/Logout/LogoutClient.js, Version/Default/index.js, Version/Restore/index.js, Version/SelectComparison/VersionDrawer/CreatedAtCell.js
+  useModal: H,                 // imported by views/Account/ResetPreferences/index.js, Dashboard/Default/ModularDashboard/*, Version/Restore/index.js, Version/SelectComparison/VersionDrawer/index.js
+  useDocumentTitle: H,         // imported by views/Version/Default/SetStepNav.js
+  useEditDepth: H              // imported by views/Version/SelectComparison/VersionDrawer/index.js
 };
 const vals = {
   formatDate: function (d) { return d }, formatDateTime: function (d) { return d },
@@ -57,7 +87,13 @@ const vals = {
   fieldHasSubFields: function () { return false },
   fieldIsBlockType: function () { return false },
   toast: function () { },
-  RenderServerComponent: function (p) { return p && p.Component ? React.createElement(p.Component, p) : null }
+  RenderServerComponent: function (p) { return p && p.Component ? React.createElement(p.Component, p) : null },
+  // --- Missing values/utilities identified by auditing all @davincios/ui imports across packages/next/dist ---
+  formatTimeToNow: function () { return '' },         // imported by views/Version/Default/index.js
+  escapeDiffHTML: function (s) { return s },           // imported by Version/RenderFieldsToDiff/fields/Date|Select|Text/index.js
+  unescapeDiffHTML: function (s) { return s },         // imported by Version/RenderFieldsToDiff/fields/Date|Select|Text/index.js
+  getHTMLDiffComponents: function () { return [] },    // imported by Version/RenderFieldsToDiff/fields/Date|Select|Text/index.js
+  fieldBaseClass: ''                                   // imported by views/Version/SelectComparison/index.js
 };
 
 for (const [k, v] of Object.entries({ ...comps, ...hooks, ...vals })) {
@@ -80,6 +116,6 @@ wf('utilities/getClientConfig.js', "'use strict';function getClientConfig(){retu
 wf('forms/fieldSchemasToFormState.js', "'use strict';exports.fieldSchemasToFormState=function(s){return{}};\n");
 wf('utilities/buildFieldSchemaMap/traverseFields.js', "'use strict';exports.traverseFields=function(){};\n");
 wf('assets.js', "'use strict';exports.davinciosFaviconDark='';exports.davinciosFaviconLight='';exports.staticOGImage='';\n");
-wf('shared.js', "'use strict';const React=require('react');exports.DaVinciOSIcon=function(){return null};exports.DaVinciOSLogo=function(){return null};exports.EntityType={};exports.findLocaleFromCode=function(){};exports.abortAndIgnore=function(){};exports.handleAbortRef=function(){};\n");
+wf('shared.js', "'use strict';const React=require('react');exports.DaVinciOSIcon=function(){return null};exports.DaVinciOSLogo=function(){return null};exports.EntityType={};exports.findLocaleFromCode=function(){};exports.abortAndIgnore=function(){};exports.handleAbortRef=function(){};exports.getVisibleEntities=function(){return[]};\n");
 
 console.log('Created @davincios/ui with ' + Object.keys(comps).length + ' components, ' + Object.keys(hooks).length + ' hooks, ' + Object.keys(vals).length + ' values');
